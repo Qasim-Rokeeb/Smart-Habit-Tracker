b2facd30778b1bc5c323b0b1203de6b207b43f7e
Qasim Rokeeb <qasimrokeeb@gmail.com>
2025-07-18 07:19:28 +0100

    day 16

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {
-        brandDark: "#0b0b0d",
-        brandBlue: "#125795",
-      },
-      fontFamily :{
-        merriweather: ["Merriweather", 'serif'],
-        montserrat: ["Montserrat", 'sans-serif'],
-      },
-    },
+      animation: { 'fade-in': 'fadeIn .4s ease-out forwards' },
+      keyframes: { fadeIn: { '0%': { opacity: 0 }, '100%': { opacity: 1 } } }
+    }
   },
   plugins: [],
 }
-
